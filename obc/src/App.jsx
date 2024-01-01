705b750577a617aeaf3ec3a8b554ff32bfd6d7e3
Luan <luancostar@gmail.com>
2024-01-01 18:28:30 -0300

    first commit

--- obc/src/App.jsx
+++ obc/src/App.jsx
@@ -1,7 +1,41 @@
 export default function App() {
   return (
     <div>
-      <h1>Olá! galera</h1>
+      <img width="100" height="100" src="../public/react.png" alt="" />
+      <h1>React</h1>
+      <p>A biblioteca para interface de usuários web nativa</p>
+      <div className="buttons">
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+      </div>
+      <hr />
+      <div>
+      <h1>Crie interfaces de usuários de componentes</h1>
+      <p>Componentes react são funções javascript. a sintaxe de marcação é chamada de JSX é uma extensão de sintaxa JS popularizada pelo React</p>
+ 
+      <hr />
+      
+    </div>
+    <div>
+      <h1>Crie interfaces de usuários de componentes</h1>
+      <p>Componentes react são funções javascript. a sintaxe de marcação é chamada de JSX é uma extensão de sintaxa JS popularizada pelo React</p>
+ 
+      <hr />
+      
+    </div>
+    <div>
+      <h1>Próximos Passos</h1>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX </li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de Componentes</li>
+        <li>Reutilização de Componentes</li>
+        <li>Uso de props e childrens</li>
+        <li>Uso de eventos do JS</li>
+
+      </ul>
+ 
+    </div>
     </div>
   )
 }
