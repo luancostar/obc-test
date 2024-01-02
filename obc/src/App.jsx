1b26b3d934598569c2e78562017a53862505e518
Luan <luancostar@gmail.com>
2024-01-01 22:43:23 -0300

    first commit

--- obc/src/App.jsx
+++ obc/src/App.jsx
@@ -1,41 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+function sum(a, b){
+  return a + b
+}
+
 export default function App() {
+  const tech = "Javascript"
+  const status = true
+
   return (
     <div>
-      <img width="100" height="100" src="../public/react.png" alt="" />
-      <h1>React</h1>
-      <p>A biblioteca para interface de usuários web nativa</p>
-      <div className="buttons">
-        <button>Aprenda React</button>
-        <button>Referência da API</button>
-      </div>
-      <hr />
-      <div>
-      <h1>Crie interfaces de usuários de componentes</h1>
-      <p>Componentes react são funções javascript. a sintaxe de marcação é chamada de JSX é uma extensão de sintaxa JS popularizada pelo React</p>
- 
-      <hr />
-      
-    </div>
-    <div>
-      <h1>Crie interfaces de usuários de componentes</h1>
-      <p>Componentes react são funções javascript. a sintaxe de marcação é chamada de JSX é uma extensão de sintaxa JS popularizada pelo React</p>
- 
-      <hr />
-      
-    </div>
-    <div>
-      <h1>Próximos Passos</h1>
-      <ul>
-        <li>Uso de dados dinâmicos no JSX </li>
-        <li>Criação de novos componentes</li>
-        <li>Estilização de Componentes</li>
-        <li>Reutilização de Componentes</li>
-        <li>Uso de props e childrens</li>
-        <li>Uso de eventos do JS</li>
-
-      </ul>
- 
-    </div>
+      <h1>{tech} is awesome!</h1>
+      <h2>It's easy, like 1 + 1 is {sum(1, 1)}</h2>
+      <h2>Current Status: {status ? "ON" : "OFF"}</h2>
     </div>
   )
 }
