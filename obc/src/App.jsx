2bcc51c3645b4fbcc31697c36fc3780f3224fd08
Luan <luancostar@gmail.com>
2024-01-07 10:35:17 -0300

    first commit

--- obc/src/App.jsx
+++ obc/src/App.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import { Fragment } from "react"
 import Card from "./components/Card/index.jsx"
+import posterZero from "./assets/poster.jpg"
+import posterOne from "./assets/poster-2.jpg"
+import posterTwo from "./assets/poster-3.jpg"
 
 export default function App() {
 
   return (
-    <div>
-      <Card/>
-    </div>
+    <Fragment>
+      <Card poster={posterZero} title="Alien I (1979)" paragraph="In deep space, the crew of the commercial starship Nostromo is awakened from their cryo-sleep capsules halfway through their journey home to investigate a distress call from an alien vessel. The terror begins when the crew encounters a nest of eggs inside the alien ship. An organism from inside an egg leaps out and attaches itself to one of the crew, causing him to fall into a coma."/>
+      <Card poster={posterOne}  title="Alien II (1986)" paragraph="After floating in space for 57 years, Lt. Ripley's (Sigourney Weaver) shuttle is found by a deep space salvage team. Upon arriving at LV-426, the marines find only one survivor, a nine year old girl named Newt (Carrie Henn). But even these battle-hardened marines with all the latest weaponry are no match for the hundreds of aliens that have invaded the colony."
+      />
+      <Card poster={posterTwo} title="Alien III (1992)" paragraph="Ellen Ripley (Sigourney Weaver) is the only survivor when she crash lands on Fiorina 161, a bleak wasteland inhabited by former inmates of the planet's maximum security prison. Once again, Ripley must face skepticism and the alien as it hunts down the prisoners and guards. Without weapons or modern technology of any kind, Ripley leads the men into battle against the terrifying creature."/>
+ 
+
+    </Fragment>
   )
 }
--- obc/src/components/Card/index.jsx
+++ obc/src/components/Card/index.jsx
@@ -1,6 +1,6 @@
-import poster from "./../../assets/poster.jpg"
 
-export default function Card() {
+
+export default function Card( {title, paragraph, poster}) {
     return (
 
     <div 
@@ -15,8 +15,8 @@ export default function Card() {
              <img className="img--content"  src={poster} />
             </div>
             <div className="content--2">
-                <h2>Pôster: Alien (1979)</h2>
-                <p>In deep space, the crew of the commercial starship Nostromo is awakened from their cryo-sleep capsules halfway through their journey home to investigate a distress call from an alien vessel. The terror begins when the crew encounters a nest of eggs inside the alien ship. An organism from inside an egg leaps out and attaches itself to one of the crew, causing him to fall into a coma.</p>
+                <h2>{title}</h2>
+                <p>{paragraph}</p>
                 <button>Comprar agora</button>
             </div>
         </div>
